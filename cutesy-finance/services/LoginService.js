cc58ff130f22aa7db1f0482fa1238f5e9fb32980
deskiryu <49835745+deskiryu@users.noreply.github.com>
2025-07-09 15:50:40 +0100

    Implement login service and integrate

---
+++ cutesy-finance/services/LoginService.js
@@ -0,0 +1,32 @@
+const LOGIN_PATH = 'Auth/SignIn';
+
+let baseUrl = '';
+let token = null;
+
+export const setBaseUrl = (url) => {
+  baseUrl = url;
+};
+
+export const getBaseUrl = () => baseUrl;
+
+export const getToken = () => token;
+
+export const login = async (emailAddress, password) => {
+  if (!baseUrl) {
+    throw new Error('Base URL is not set');
+  }
+  const response = await fetch(`${baseUrl.replace(/\/+$/, '')}/${LOGIN_PATH}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ emailAddress, password }),
+  });
+
+  const data = await response.json();
+  if (data && data.token) {
+    token = data.token;
+  }
+  return data;
+};
+
